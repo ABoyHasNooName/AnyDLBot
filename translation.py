e23d884114cc257fd061a153dc89567b2515bf63
ABoyHasNooName <39313183+ABoyHasNooName@users.noreply.github.com>
2018-10-28 04:01:43 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,5 +1,5 @@
 
 class Translation(object):
-    START_TEXT = "Please send me any video link. Supported Sites: https://rg3.github.io/youtube-dl/supportedsites.html \nSource Code: https://github.com/SpEcHiDe/AnyDLBot"
+    START_TEXT = "Please send me any video link. Supported Sites: https://rg3.github.io/youtube-dl/supportedsites.html \nSource Bot has been cloned from: https://github.com/SpEcHiDe/AnyDLBot With Love @AbinPauIZackariah 💚"
     ABS_TEXT = " #ForTheGreaterGood "
     UPGRADE_TEXT = "No upgrades!"
