42e9c220c37bff3ef58d68e649f0829b44a0c9c5
ABoyHasNooName <39313183+ABoyHasNooName@users.noreply.github.com>
2018-10-28 04:02:47 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,21 @@
+
+class Config(object):
+    # get a token from https://chatbase.com
+    CHAT_BASE_TOKEN = "8ba613d9-d8e8-4a4d-99d1-6e905772a3c1"
+    # get a token from @BotFather
+    TG_BOT_TOKEN = "663491244:AAE_DJUPKUCBqv7aI2Grz6Hl3Rgat7HQ3mw"
+    # your domain to show when download file is greater than MAX_FILE_SIZE
+    HTTP_DOMAIN = "jithumon.in/nokkiyirunno.html"
+    # the download location, where the HTTP Server runs
+    DOWNLOAD_LOCATION = "./DOWNLOADS"
+    # Telegram maximum file upload size
+    MAX_FILE_SIZE = 50000000
+    TG_MAX_FILE_SIZE = 14000000000
+    # The Telegram API things
+    APP_ID = 604405
+    API_HASH = "dd1119c74e1879cb4accf97b8a277d92"
+    # Get these values from my.telegram.org
+    # for storing the Telethon session
+    TL_SESSION = "AnyDLBot.session"
+    CHUNK_SIZE = 128
+
